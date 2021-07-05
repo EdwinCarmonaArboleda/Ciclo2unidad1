7feda6e924db8f4c412ebdc29f8a118536425a36
EdwinCarmonaArboleda <edwin.kar088@gmail.com>
2021-07-05 18:38:04 -0500

    Prueba//

--- primerproyectojava/src/junio29c.java
+++ primerproyectojava/src/junio29c.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
 public class junio29c {
-    public static void main(String[] args)
+    public static void main(String[] args);
     Scanner sacanner = new Scanner(System.in);
     int numero = scanner.nextLine();
     int resultado calcularDigitos(numero);
     System.out.println(resultado);
 }
 
-public static int calcularDigitos(int numero){
-    int resultado = 0;
-    while (numero!=0){
+//public static int calcularDigitos(int numero){
+    //int resultado = 0;
+    //while (numero!=0){
         //numero = numero / 10;
-        numero /= 10; numero =10.0;
-        cifras++;
+        //numero /= 10; numero =10.0;
+        //cifras++;
     }
-    return cifras;
+    //return cifras;
 }
 
 
